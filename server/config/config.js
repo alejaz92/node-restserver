ee78676a0a33de268db1c2dbe77b53df89159a1c
Alejandro Jazmatie <alejandro.jaz92@gmail.com>
2019-08-06 11:17:00 -0300

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -19,7 +19,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://alejaz:qcj1jTcFP9Jwqd2C@cluster0-ppcjm.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
