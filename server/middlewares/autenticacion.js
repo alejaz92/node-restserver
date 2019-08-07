16e3a174417c7cc9f6a15d3ec6e4be06ffd28820
Alejandro Jazmatie <alejandro.jaz92@gmail.com>
2019-08-07 11:09:37 -0300

    Login normal secc 10

---
+++ server/middlewares/autenticacion.js
@@ -0,0 +1,52 @@
+const jwt = require('jsonwebtoken');
+
+
+// =====================
+// Verificar token
+// =====================
+
+let verificaToken = (req, res, next) => {
+
+    let token = req.get('token');
+
+    jwt.verify(token, process.env.SEED, (err, decoded) => {
+        if (err) {
+            return res.status(401).json({
+                ok: false,
+                err: {
+                    message: 'Token no valido'
+                }
+            });
+        }
+        req.usuario = decoded.usuario;
+        next();
+    });
+};
+
+// =====================
+// Verificar AdminRole
+// =====================
+
+let verificaAdminRole = (req, res, next) => {
+
+    let usuario = req.usuario;
+
+    if (usuario.role === 'ADMIN_ROLE') {
+        next();
+    } else {
+        return res.status(401).json({
+            ok: false,
+            err: {
+                message: 'Usuario no tiene permisos'
+            }
+        });
+    }
+
+}
+
+
+
+module.exports = {
+    verificaToken,
+    verificaAdminRole
+}
--- server/routes/usuario.js
+++ server/routes/usuario.js
@@ -3,9 +3,11 @@ const Usuario = require('../models/usuario');
 const bcrypt = require('bcrypt');
 const _ = require('underscore');
 
+const { verificaToken, verificaAdminRole } = require('../middlewares/autenticacion');
+
 const app = express();
 
-app.get('/usuario', function(req, res) {
+app.get('/usuario', verificaToken, (req, res) => {
 
     let desde = req.query.desde || 0;
     desde = Number(desde);
@@ -36,7 +38,7 @@ app.get('/usuario', function(req, res) {
         });
 });
 
-app.post('/usuario', function(req, res) {
+app.post('/usuario', [verificaToken, verificaAdminRole], (req, res) => {
 
     let body = req.body;
 
@@ -63,12 +65,12 @@ app.post('/usuario', function(req, res) {
     });
 });
 
-app.put('/usuario/:id', function(req, res) {
+app.put('/usuario/:id', [verificaToken, verificaAdminRole], (req, res) => {
 
     let id = req.params.id;
     let body = _.pick(req.body, ['nombre', 'email', 'img', 'role', 'estado']);
 
-    Usuario.findOneAndUpdate(id, body, { new: true, runValidators: true }, (err, usuarioDB) => {
+    Usuario.findByIdAndUpdate(id, body, { new: true, runValidators: true }, (err, usuarioDB) => {
         if (err) {
             return res.status(400).json({
                 ok: false,
@@ -83,7 +85,7 @@ app.put('/usuario/:id', function(req, res) {
 
 });
 
-app.delete('/usuario/:id', function(req, res) {
+app.delete('/usuario/:id', [verificaToken], (req, res) => {
 
     let id = req.params.id;
 
@@ -92,7 +94,7 @@ app.delete('/usuario/:id', function(req, res) {
     };
 
     //    Usuario.findByIdAndRemove(id, (err, usuarioBorrado) => {
-    Usuario.findOneAndUpdate(id, cambiaEstado, { new: true }, (err, usuarioBorrado) => {
+    Usuario.findByIdAndUpdate(id, cambiaEstado, { new: true }, (err, usuarioBorrado) => {
         if (err) {
             return res.status(400).json({
                 ok: false,
