bf5148e7b66c4201732c768a06f5dead39fb302c
Alejandro Jazmatie <alejandro.jaz92@gmail.com>
2019-08-08 16:12:04 -0300

    Google sign in

--- server/config/config.js
+++ server/config/config.js
@@ -41,3 +41,9 @@ process.env.CADUCIDAD_TOKEN = 60 * 60 * 24 * 30;
 // =====================
 
 process.env.SEED = process.env.SEED || 'este-es-el-seed-desarrollo';
+
+// =====================
+// Google Client ID
+// =====================
+
+process.env.CLIENT_ID = process.env.CLIENT_ID || '58618283651-i7tfht5n2h6ceudmvgf9qsus7gh8i2kv.apps.googleusercontent.com';
--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ require('./config/config');
 
 const express = require('express');
 const mongoose = require('mongoose');
+const path = require('path');
 
 const app = express();
 const bodyParser = require('body-parser');
@@ -12,7 +13,10 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
 app.use(bodyParser.json());
 
-// COnfiguracion global de rutas
+// Habilitar la carpeta public
+app.use(express.static(path.resolve(__dirname, '../public')));
+
+// Configuracion global de rutas
 app.use(require('./routes/index'));
 
 mongoose.connect(process.env.URLDB, { useNewUrlParser: true, useCreateIndex: true }, (err, res) => {
